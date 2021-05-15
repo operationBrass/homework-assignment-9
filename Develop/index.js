a8ae9a3948b46bd4080a30343691c32de3816255
BRASS <mr.brn.lewis@outlook.com>
2021-05-15 16:50:55 +0800

    import inquirer, create questions, process questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,49 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{type: "input", 
+                    name:"Title", 
+                    message:"Please enter a title: "},
+                {
+                    type: "input", 
+                    name:"Description", 
+                    message:"Please enter a description: "
+                },
+                {
+                    type: "input", 
+                    name:"install", 
+                    message:"Please enter installation instructions: "
+                }, 
+                {
+                    type: "input", 
+                    name:"usage", 
+                    message:"Please enter usage information: "
+                },
+                {
+                    type: "input", 
+                    name:"test", 
+                    message:"Please enter test instructions: "
+                }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions)
+
+    .then(answers => {
+
+
+    })
+    .catch(error => {
+
+    });
+
+}
 
 // Function call to initialize app
 init();
